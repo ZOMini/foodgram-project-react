9060a858715b04f04e13ed01ff5919862080156b
ZOMini <zarikacr@gmail.com>
2022-05-26 20:43:00 +0300

    v 2.9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
