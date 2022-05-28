b11083a22f0cd61c62635149b3fbd7f3f923e856
ZOMini <zarikacr@gmail.com>
2022-05-28 04:41:40 +0300

    v 3.6

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -55,7 +55,7 @@ class RecipeFilters(FilterSet):
             return queryset
         try:
             recipes = (
-                self.request.user.purchases.recipes.all()
+                self.request.user.purchases.recipe.all()
             )
         except Cart.DoesNotExist:
             return queryset
